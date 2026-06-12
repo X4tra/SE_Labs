6be9be7c96560969bfc91582a8179e77b645b59c
X4tra <63122131+X4tra@users.noreply.github.com>
2026-06-12 10:41:39 +0200

    feature: added safety for COOLING compressor safety

--- Project/thermostat.py
+++ Project/thermostat.py
@@ -2,15 +2,17 @@ class ThermostatController:
     def __init__(self, target: float, hysteresis: float):
         self.target = target
         self.hysteresis = hysteresis
+        self.current_state = "IDLE"
 
-def evaluate(self, current_temp: float) -> str:
+    def evaluate(self, current_temp: float) -> str:
         if current_temp <= (self.target - self.hysteresis):
             self.current_state = "HEATING"
         elif self.current_state == "HEATING" and current_temp >= self.target:
             self.current_state = "IDLE"
             
-        # temp
-        elif self.current_state == "IDLE" and current_temp > self.target:
-            return "COOLING"
-            
+        elif current_temp >= (self.target + self.hysteresis):
+            self.current_state = "COOLING"
+        elif self.current_state == "COOLING" and current_temp <= self.target:
+            self.current_state = "IDLE"
+
         return self.current_state
